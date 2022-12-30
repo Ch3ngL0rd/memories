2ffc6ee19f2dc5e5a151e90a78813dd3a79d2af2
Muffin-Man2 <85885968+Muffin-Man2@users.noreply.github.com>
2022-12-30 23:17:15 +0800

    PLease go on aptop

--- pages/mobile.tsx
+++ pages/mobile.tsx
@@ -21,7 +21,7 @@ export default function Mobile() {
         <div className="font-primary flex flex-col items-center justify-center h-screen">
             <p className="text-4xl text-center absolute top-5 border-b border-black">Memory of Yesterday</p>
             <p className="text-4xl text-center">Sorry...mobile isn&apos;t supported yet.</p>
-            <p className="text-4xl text-center">Come back later! 😁</p>
+            <p className="text-4xl text-center">Please go on your laptop! 😁</p>
         </div>
     )
 }
